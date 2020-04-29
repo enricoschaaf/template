abb942887704c30729d245c78bc6ca639bec389e
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-04-30 01:40:00 +0200

    update to tailwind 1.4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
+  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
