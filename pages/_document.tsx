e92aa076facd78340b3b0481516c8e079e4b544c
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-06-02 15:09:10 +0200

    remove inter and create new branch

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,13 +8,6 @@ class MyDocument extends Document {
           <meta name="description" content="Meta description" />
           <meta name="theme-color" content="#f4f5f7" />
           <link rel="icon" type="image/svg+xml" href="favicon.svg" />
-          <link
-            rel="preload"
-            href="https://rsms.me/inter/font-files/Inter.var.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
         </Head>
         <body className="bg-gray-100 text-gray-900">
           <Main />
