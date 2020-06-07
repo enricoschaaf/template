01d86ded79a7500beb0e434cf0a9b9d2790a5800
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-06-07 13:50:22 +0200

    fix inter font issue, remove polyfills, optimize preact usage

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,13 @@ class MyDocument extends Document {
           <meta name="description" content="Meta description" />
           <meta name="theme-color" content="#f4f5f7" />
           <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+          <link
+            rel="preload"
+            as="font"
+            href="/Inter.var.latin.woff2"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body className="bg-gray-100 text-gray-900">
           <Main />
